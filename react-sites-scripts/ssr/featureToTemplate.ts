c78b1f12703fca87c5d9d5f0c556f4634ece92b9
Matt Kilpatrick <mkilpatrick@yext.com>
2022-03-09 18:07:10 -0500

    Handle template config name changes upon page refresh

--- react-sites-scripts/ssr/featureToTemplate.ts
+++ react-sites-scripts/ssr/featureToTemplate.ts
@@ -5,8 +5,14 @@ export const featureToTemplate = async (feature: string): Promise<string> => {
     const dir = await readdir(`./${TEMPLATE_PATH}`);
     let templateFilename = 'index.tsx'; // fallback to index page
     for (const fileName of dir) {
-        const component = await import(`../../${TEMPLATE_PATH}/${fileName}`);
-        if (feature === component.config?.name.replace(/\s/g,'').toLowerCase()) {
+        const filepath = `../../${TEMPLATE_PATH}/${fileName}`;
+
+        // Delete the cached module so a page refresh gets the updated module data (such as a change to the config's name)
+        delete require.cache[require.resolve(filepath)];
+
+        const component = await import(filepath);
+
+        if (feature === component.config?.name?.replace(/\s/g,'').toLowerCase()) {
             templateFilename = fileName;
             break;
         }
