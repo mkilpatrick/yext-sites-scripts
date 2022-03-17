d18fc509d97895f4e79cebdc562b18469bc0d46e
Matt Kilpatrick <mkilpatrick@yext.com>
2022-03-17 12:28:36 -0600

    Inline vite config for export

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer as createViteServer } from "vite";
 import { serverRenderRoute } from "./yext-sites-scripts/ssr/serverRenderRoute";
 import { getServerSideProps } from "./yext-sites-scripts/ssr/getServerSideProps";
+import react from '@vitejs/plugin-react';
 
 export const createServer = async () => {
   // creates a standard express app
@@ -10,6 +11,13 @@ export const createServer = async () => {
   // create vite using ssr mode
   const vite = await createViteServer({
     server: { middlewareMode: "ssr" },
+    plugins: [
+      react()
+    ],
+    // Temporary solution https://github.com/vitejs/vite/issues/6215
+    optimizeDeps: {
+      include: ['react/jsx-runtime'],
+    }
   });
   // register vite's middleware
   app.use(vite.middlewares);
