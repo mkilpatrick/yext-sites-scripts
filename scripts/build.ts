96e8ebac35a5fa35e46cfe9ad1d6d45f5a88eea5
Matt Kilpatrick <mkilpatrick@yext.com>
2022-03-29 15:44:09 -0600

    Add ssg-plugin d.ts

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,9 +1,7 @@
 #! /usr/bin/env node
 import { build } from 'vite'
 import react from '@vitejs/plugin-react';
-/* eslint-disable */
 import yextSSG from 'ssg-plugin';
-/* eslint-enable */
 
 (async () => {
   await build({
