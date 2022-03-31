f02de43523522ec8d15f8319e4cdedb463cac248
Justin Mancusi <11779614+mancusi@users.noreply.github.com>
2022-03-31 17:15:18 -0400

    fix error message formatting for {featureName}/entityId}

--- yext-sites-scripts/ssr/urlToFeature.ts
+++ yext-sites-scripts/ssr/urlToFeature.ts
@@ -1,9 +1,11 @@
+import chalk from "chalk";
+
 export const urlToFeature = (url: string): { feature: string; entityId: string } => {
   // URI decode and remove leading slash: /foo/123
   const uriSegments = decodeURI(url).substring(1).split('/');
 
   if (uriSegments.length !== 2) {
-    process.stderr.write('Url must be of the form /{featureName}/{entityId}');
+    process.stderr.write(`Url must be of the form ${chalk.bold('/{featureName}/{entityId}')}\n`);
   }
 
   // Remove all whitespaces and lowercase
