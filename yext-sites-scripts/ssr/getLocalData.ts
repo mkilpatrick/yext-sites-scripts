59852182b87feca15b1b412c355df28837994887
Matt Kilpatrick <mkilpatrick@yext.com>
2022-04-03 16:40:36 -0600

    Update prop name from uid to entityId to make it more clear

--- yext-sites-scripts/ssr/getLocalData.ts
+++ yext-sites-scripts/ssr/getLocalData.ts
@@ -4,13 +4,13 @@ import { readdir } from 'fs/promises';
 
 const LOCAL_DATA_PATH = 'localData';
 
-export const getLocalData = async (uid: string) => {
+export const getLocalData = async (entityId: string) => {
   const dir = await readdir(LOCAL_DATA_PATH);
 
   for (const fileName of dir) {
     const data = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), `${LOCAL_DATA_PATH}/${fileName}`)).toString());
 
-    if (data.id?.toString() === uid) {
+    if (data.id?.toString() === entityId) {
       return data;
     }
   }
