69a2e7cbb27da333ac0d93e851efe22717531fbd
Matt Kilpatrick <mkilpatrick@yext.com>
2022-04-08 10:16:51 -0400

    Get dynamic generateTestData working

--- yext-sites-scripts/ssr/generateTestData.ts
+++ yext-sites-scripts/ssr/generateTestData.ts
@@ -1,22 +1,48 @@
 import { spawn } from 'child_process';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const generateTestData = async (featureConfig: any, entityId: string) => {
-  const generateTestDataExec = spawn('yext', [
-    'sites',
-    'generate-test-data',
-    '--featureName',
-    featureConfig?.features[0]?.name,
-    '--entityId',
-    entityId,
-    '--featuresConfig',
-    JSON.stringify(featureConfig),
-    '--locale',
-    'en',
-    // '--printDocuments',
-  ]);
+export const generateTestData = async (featureConfig: any, entityId: string): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    const generateTestDataExec = spawn('yext', [
+      'sites',
+      'generate-test-data',
+      '--featureName',
+      `'${featureConfig?.features[0]?.name}'`,
+      '--entityId',
+      entityId,
+      '--featuresConfig',
+      `'${JSON.stringify(featureConfig)}'`,
+      '--locale',
+      'en',
+      '--printDocuments',
+    ], {
+      stdio: ["pipe", "pipe", "pipe"],
+      shell: true,
+    });
 
-  generateTestDataExec.stdout.pipe(process.stdout);
-  generateTestDataExec.stderr.pipe(process.stderr);
-  process.stdin.pipe(generateTestDataExec.stdin);
+    let testData = '';
+
+    generateTestDataExec.stdout.setEncoding('utf8');
+    generateTestDataExec.stdout.on('data', (chunk) => {
+      if (chunk.startsWith('{')) {
+        testData += chunk;
+      } else if (chunk.includes('This is a beta version of the Yext Command Line Interface')) {
+        return;
+      } else {
+        process.stdout.write(chunk);
+      }
+    });
+
+    generateTestDataExec.stderr.setEncoding('utf8');
+    generateTestDataExec.stderr.on('data', (chunk) => {
+      process.stderr.write(chunk);
+    });
+
+    generateTestDataExec.on('close', (code, signal) => {
+      testData = JSON.parse(testData.trim());
+      resolve(testData);
+    });
+
+    process.stdin.pipe(generateTestDataExec.stdin);
+  });
 };
