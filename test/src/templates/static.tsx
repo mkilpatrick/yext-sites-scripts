72c1b4fc04203bd2c1219dcb877721040589889e
Matt Kilpatrick <mkilpatrick@yext.com>
2022-04-13 12:38:00 -0400

    Add getStaticProps to example static file

--- test/src/templates/static.tsx
+++ test/src/templates/static.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import fetch from "cross-fetch";
 
 export const config = {
   name: 'static',
@@ -8,15 +9,24 @@ export const getPath = (data: any) => {
   return `static/${Math.random().toString()}`;
 };
 
-const Static = (props: any) => {
-  const { document } = props.data;
-  console.log(JSON.stringify(document));
+export const getStaticProps: any = async () => {
+  const url = `https://pokeapi.co/api/v2/pokemon/1`;
+  const pokemon = await fetch(url)
+    .then(res => res.json());
+
+  return pokemon;
+};
+
+const Static = ({ data }: { data: any }) => {
+  const { document } = data;
+  const { streamOutput } = document;
+  const { name } = streamOutput;
 
   const [num, setNum] = useState<number>(0);
 
   return (
     <>
-      <div>Hello from static</div>
+      <div>Hello from {name}</div>
       <button onClick={() => setNum(num + 1)}>Click me</button>
       Num: {num}
     </>
